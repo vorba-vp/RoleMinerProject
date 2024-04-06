7b9c07054e97a1f90a8ed225ccb0b69fbc8a557c
Vladimir Ponomarev <vorba.vp@gmail.com>
2024-04-06 21:37:02 +0300

    added loading upa from file

--- upa_matrix_builder.py
+++ upa_matrix_builder.py
@@ -1,33 +1,45 @@
 import re
+import time
+
+import numpy as np
 
 
 class UpaMatrixBuilder:
     @classmethod
-    def load_from_one2one_file(cls, filename: str):
-        num_of_users = 0
-        num_of_permissions = 0
+    def load_from_one2one_file(cls, filename: str) -> np.ndarray:
+        num_of_users, num_of_permissions = 0, 0
+
+        # Using a set to hold unique user and permission numbers for calculating the size of the matrix
+        user_set, permission_set = set(), set()
+
         with open(filename, "r") as file:
             for line in file:
-                user_num, permission_num = re.sub(r"\s+", " ", line.strip()).split(" ")
-                num_of_users = (
-                    int(user_num) if int(user_num) > num_of_users else num_of_users
+                user_num, permission_num = map(
+                    int, re.sub(r"\s+", " ", line.strip()).split(" ")
                 )
-                num_of_permissions = (
-                    int(permission_num)
-                    if int(permission_num) > num_of_permissions
-                    else num_of_permissions
+                user_set.add(user_num)
+                permission_set.add(permission_num)
+
+        num_of_users = max(user_set)
+        num_of_permissions = max(permission_set)
+
+        upa_matrix = np.zeros((num_of_users, num_of_permissions))
+
+        with open(filename, "r") as file:
+            for line in file:
+                user_num, permission_num = map(
+                    int, re.sub(r"\s+", " ", line.strip()).split(" ")
                 )
+                upa_matrix[user_num - 1][permission_num - 1] = 1
 
-            file.seek(0)
-
-        print(f"Users: {num_of_users}, Permissions: {num_of_permissions}")
+        return upa_matrix
 
 
 if __name__ == "__main__":
-    import time
-
     start_time = time.time()
 
-    UpaMatrixBuilder.load_from_one2one_file("real_datasets/americas_large.txt")
+    matrix = UpaMatrixBuilder.load_from_one2one_file("real_datasets/healthcare.txt")
+    print(matrix)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
+    elapsed_time = time.time() - start_time
+    print(f"--- {elapsed_time} seconds ---")
