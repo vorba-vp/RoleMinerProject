7bf485a27ee8daaaf783480df19bf1076639e328
Vladimir Ponomarev <vorba.vp@gmail.com>
2024-04-07 09:31:05 +0300

    added test for matrix from final work

--- tests/test_upa_builder.py
+++ tests/test_upa_builder.py
@@ -18,6 +18,30 @@ def test__load_from_one2one_file__identity_matrix():
     assert np.array_equal(test_matrix, np.eye(5))
 
 
+def test__load_from_one2one_file__simple_dataset():
+    test_matrix_1 = UpaMatrixBuilder.load_from_one2one_file(
+        "../test_datasets/simple_dataset.txt"
+    )
+    test_matrix_2 = np.array(
+        [
+            [1, 1, 0, 1],
+            [0, 1, 1, 0],
+            [1, 1, 0, 1],
+            [1, 1, 0, 1],
+            [0, 1, 1, 1],
+            [0, 1, 1, 1],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [1, 1, 0, 1],
+            [1, 1, 0, 1],
+            [0, 1, 1, 1],
+        ]
+    )
+    assert np.array_equal(test_matrix_1, test_matrix_2)
+
+
 def test__load_from_one2one_file__non_existing_file():
     with pytest.raises(FileNotFoundError):
         UpaMatrixBuilder.load_from_one2one_file("../test_datasets/not_exist.txt")
