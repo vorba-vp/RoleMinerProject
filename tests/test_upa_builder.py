424f2a60b93d251ccfc705ede4c9ffceb9086310
Vladimir Ponomarev <vorba.vp@gmail.com>
2024-05-19 22:46:17 +0300

    refactor + tests to roles

--- tests/test_upa_builder.py
+++ tests/test_upa_builder.py
@@ -1,27 +1,26 @@
+import configparser
+
 import numpy as np
 import pytest
 
-from upa_matrix_builder import UpaMatrixBuilder
+from upa_matrix_builder import generate_pa_matrix, load_upa_from_one2one_file
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 
 def test__load_from_one2one_file__empty_file():
-    test_matrix = UpaMatrixBuilder.load_from_one2one_file(
-        "../test_datasets/empty_dataset.txt"
-    )
+    test_matrix = load_upa_from_one2one_file("test_datasets/empty_dataset.txt")
     assert np.array_equal(test_matrix, np.zeros((0, 0)))
 
 
 def test__load_from_one2one_file__identity_matrix():
-    test_matrix = UpaMatrixBuilder.load_from_one2one_file(
-        "../test_datasets/identity_matrix.txt"
-    )
+    test_matrix = load_upa_from_one2one_file("test_datasets/identity_matrix.txt")
     assert np.array_equal(test_matrix, np.eye(5))
 
 
 def test__load_from_one2one_file__simple_dataset():
-    test_matrix_1 = UpaMatrixBuilder.load_from_one2one_file(
-        "../test_datasets/simple_dataset.txt"
-    )
+    test_matrix_1 = load_upa_from_one2one_file("test_datasets/simple_dataset.txt")
     test_matrix_2 = np.array(
         [
             [1, 1, 0, 1],
@@ -44,4 +43,21 @@ def test__load_from_one2one_file__simple_dataset():
 
 def test__load_from_one2one_file__non_existing_file():
     with pytest.raises(FileNotFoundError):
-        UpaMatrixBuilder.load_from_one2one_file("../test_datasets/not_exist.txt")
+        load_upa_from_one2one_file("test_datasets/not_exist.txt")
+
+
+def test__generate_pa_matrix():
+    max_permissions_per_role = 8
+    num_of_roles = 10
+    num_of_permissions = 12
+    min_permissions_per_role = config.getint("permissions", "min_per_role")
+    roles = generate_pa_matrix(
+        num_of_roles=num_of_roles,
+        num_of_permissions=num_of_permissions,
+        max_permissions_per_role=max_permissions_per_role,
+    )
+    assert isinstance(roles, np.ndarray)
+    assert len(roles) == num_of_roles
+    for role in roles:
+        print(f"Role: {role}  Sum:{sum(role)}")
+        assert min_permissions_per_role <= sum(role) <= max_permissions_per_role
--- upa_matrix_builder.py
+++ upa_matrix_builder.py
@@ -1,69 +1,74 @@
+import configparser
 import random
 import re
 import time
 
 import numpy as np
 
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-class UpaMatrixBuilder:
-    @staticmethod
-    def load_from_one2one_file(filename: str) -> np.ndarray:
-        num_of_users, num_of_permissions = 0, 0
+MIN_PERMISSIONS_PER_ROLE = config.getint("permissions", "min_per_role")
 
-        # Using a set to hold unique user and permission numbers for calculating the size of the matrix
-        user_set, permission_set = set(), set()
 
-        with open(filename, "r") as file:
-            for line in file:
-                user_num, permission_num = map(
-                    int, re.sub(r"\s+", " ", line.strip()).split(" ")
-                )
-                user_set.add(user_num)
-                permission_set.add(permission_num)
-        if not user_set or not permission_set:
-            return np.zeros((0, 0), dtype=int)
-        num_of_users = max(user_set)
-        num_of_permissions = max(permission_set)
+def load_upa_from_one2one_file(filename: str) -> np.ndarray:
 
-        upa_matrix = np.zeros((num_of_users, num_of_permissions), dtype=int)
+    # Using a set to hold unique user and permission numbers for calculating the size of the matrix
+    user_set, permission_set = set(), set()
 
-        with open(filename, "r") as file:
-            for line in file:
-                user_num, permission_num = map(
-                    int, re.sub(r"\s+", " ", line.strip()).split(" ")
-                )
-                upa_matrix[user_num - 1][permission_num - 1] = 1
+    with open(filename, "r") as file:
+        for line in file:
+            user_num, permission_num = map(
+                int, re.sub(r"\s+", " ", line.strip()).split(" ")
+            )
+            user_set.add(user_num)
+            permission_set.add(permission_num)
+    if not user_set or not permission_set:
+        return np.zeros((0, 0), dtype=int)
+    num_of_users = max(user_set)
+    num_of_permissions = max(permission_set)
 
-        return upa_matrix
+    upa_matrix = np.zeros((num_of_users, num_of_permissions), dtype=int)
 
-    @staticmethod
-    def generate_matrix(
-        num_of_roles: int,
-        num_of_users: int,
-        num_of_permissions: int,
-        max_roles_per_user: int,
-        max_permissions_per_role: int,
-    ) -> np.ndarray:
-        roles = UpaMatrixBuilder._generate_pa_matrix(
-            num_of_roles, num_of_permissions, max_permissions_per_role
-        )
+    with open(filename, "r") as file:
+        for line in file:
+            user_num, permission_num = map(
+                int, re.sub(r"\s+", " ", line.strip()).split(" ")
+            )
+            upa_matrix[user_num - 1][permission_num - 1] = 1
 
-        for idx, role in enumerate(roles):
-            print(f"Role {idx + 1}: {role}")
+    return upa_matrix
 
-    @staticmethod
-    def _generate_pa_matrix(
-        num_of_roles: int, num_of_permissions: int, max_permissions_per_role: int
-    ) -> np.ndarray:
-        roles = np.zeros((num_of_roles, num_of_permissions), dtype=int)
-        for r in range(num_of_roles):
-            nrt = random.randint(1, max_permissions_per_role)
-            role = np.zeros(num_of_permissions, dtype=int)
 
-            random_positions = random.sample(range(num_of_permissions), nrt)
-            for pos in random_positions:
-                roles[r, pos] = 1
-        return roles
+def generate_matrix(
+    num_of_roles: int,
+    num_of_users: int,
+    num_of_permissions: int,
+    max_roles_per_user: int,
+    max_permissions_per_role: int,
+) -> np.ndarray:
+    roles = generate_pa_matrix(
+        num_of_roles=num_of_roles,
+        num_of_permissions=num_of_permissions,
+        max_permissions_per_role=max_permissions_per_role,
+    )
+
+    for idx, role in enumerate(roles):
+        print(f"Role {idx + 1}: {role}")
+
+
+def generate_pa_matrix(
+    *, num_of_roles: int, num_of_permissions: int, max_permissions_per_role: int
+) -> np.ndarray:
+    roles = np.zeros((num_of_roles, num_of_permissions), dtype=int)
+    for r in range(num_of_roles):
+        nrt = random.randint(MIN_PERMISSIONS_PER_ROLE, max_permissions_per_role)
+        role = np.zeros(num_of_permissions, dtype=int)
+
+        random_positions = random.sample(range(num_of_permissions), nrt)
+        for pos in random_positions:
+            roles[r, pos] = 1
+    return roles
 
 
 if __name__ == "__main__":
@@ -71,7 +76,7 @@ if __name__ == "__main__":
 
     # matrix = UpaMatrixBuilder.load_from_one2one_file("real_datasets/healthcare.txt")
     # print(matrix)
-    UpaMatrixBuilder.generate_matrix(5, 12, 7, 1, 4)
+    generate_matrix(5, 12, 7, 1, 4)
 
     elapsed_time = time.time() - start_time
     print(f"--- {elapsed_time} seconds ---")
