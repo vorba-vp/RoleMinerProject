a7bbdc7cf63ebed64281d744b2ceeb1d8469f273
Vladimir Ponomarev <vorba.vp@gmail.com>
2024-10-20 22:23:47 +0300

    ui fix

--- interface/layouts/control_panel.py
+++ interface/layouts/control_panel.py
@@ -1,5 +1,4 @@
 import dash_daq as daq
-from click import style
 from dash import dcc, html
 
 control_panel_layout = html.Div(
@@ -14,6 +13,7 @@ control_panel_layout = html.Div(
                             children="Algorithms Inputs",
                             style={
                                 "margin-left": "30px",
+                                "margin-right": "5px",
                                 "display": "flex",
                                 "justify-content": "center",
                                 "align-items": "center",
@@ -23,10 +23,19 @@ control_panel_layout = html.Div(
                             id="dataset-dropdown",
                             options=[
                                 {"label": "Simple Dataset", "value": "simple_dataset"},
-                                {"label": "Identity Matrix", "value": "identity_matrix"},
+                                {
+                                    "label": "Identity Matrix",
+                                    "value": "identity_matrix",
+                                },
                                 {"label": "Healthcare Dataset", "value": "healthcare"},
-                                {"label": "Americas Large Dataset", "value": "americas_large"},
-                                {"label": "Americas Small Dataset", "value": "americas_small"},
+                                {
+                                    "label": "Americas Large Dataset",
+                                    "value": "americas_large",
+                                },
+                                {
+                                    "label": "Americas Small Dataset",
+                                    "value": "americas_small",
+                                },
                                 {"label": "APJ Dataset", "value": "apj"},
                                 {"label": "Customer Dataset", "value": "customer"},
                                 {"label": "Domino Dataset", "value": "domino"},
@@ -39,7 +48,7 @@ control_panel_layout = html.Div(
                         ),
                         html.Span(
                             id="d-factor-label",
-                            children="RMP \u03B4 -Factor:",
+                            children="RMP \u03b4 -Factor:",
                             style={
                                 "margin-left": "30px",
                                 "display": "flex",
@@ -57,7 +66,11 @@ control_panel_layout = html.Div(
                 ),
                 html.Div(
                     children=[
-                        html.Button("Show UPA", id="show-upa-button", style={"margin-left": "30px"}),
+                        html.Button(
+                            "Show UPA",
+                            id="show-upa-button",
+                            style={"margin-left": "30px"},
+                        ),
                         html.Button(
                             "Show FastMiner Result",
                             id="show-fm-button",
@@ -75,12 +88,15 @@ control_panel_layout = html.Div(
                             style={"margin-left": "30px"},
                         ),
                     ],
-                    style={"display": "flex", "flex-direction": "row", "margin-top": "10px"},
+                    style={
+                        "display": "flex",
+                        "flex-direction": "row",
+                        "margin-top": "10px",
+                    },
                 ),
             ],
             style={"display": "flex", "flex-direction": "column"},
         ),
-
         html.Div(id="warning-message", style={"color": "red", "margin-top": "10px"}),
     ],
     style={"height": "15vh"},
